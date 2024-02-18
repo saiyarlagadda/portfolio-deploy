fe05708359155bb7006309bc43762915efe0214d
saiyarlagadda <77604392+saiyarlagadda@users.noreply.github.com>
2024-02-18 17:38:52 -0500

    footer changes

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 
+import {footer} from'../Footer/footer.css'
 export default class Footer extends Component {
   render() {
     return (
-      <div>Footer</div>
+        <>
+        <footer className='footer'>
+            <div className='word'>Crafted by Sai Yarlagadda</div>
+            {/* <div className='link'>
+                <div className='home-mini'>Home</div>
+                <div className='about-mini'>about</div>
+            </div> */}
+        </footer>
+        </>
     )
   }
 }
