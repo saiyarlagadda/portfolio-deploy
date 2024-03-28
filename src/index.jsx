1d4cb51012d8c01805d9fdaf8b9aeca8f9ae280d
saiyarlagadda <77604392+saiyarlagadda@users.noreply.github.com>
2024-03-28 15:49:44 -0400

    Firebase- deployed

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// // import './index.css';
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+
+//     <App />
+ 
+// );
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-
+// Create a root using ReactDOM.createRoot
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
 
-    <App />
- 
-);
+// Render your main component inside the root
+root.render(<App />);
